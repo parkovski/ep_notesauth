c136cc2a11b5845470bb264f61745d4ac4234989
Parker Snell <parkersnell@gmail.com>
2014-04-01 17:03:07 -0700

    ep usernames match notes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('request');
 
+var ERR = require('async-stacktrace');
 var settings = require('ep_etherpad-lite/node/utils/Settings');
 var authorManager = require('ep_etherpad-lite/node/db/AuthorManager');
 
@@ -15,6 +16,24 @@ var whitelist = [
 function isWhitelisted(path) {
   return whitelist.some(function(resource) {
     return path === resource;
+  });
+}
+
+// setUsername and handleMessage from ep_sotauth/ep_ldapauth.
+function setUsername(token, username) {
+  console.debug('ep_notesauth.setUsername: getting authorid for token %s', token);
+  authorManager.getAuthor4Token(token, function(err, author) {
+    if(ERR(err)) {
+      console.debug('ep_notesauth.setUsername: error getting author for token %s', token);
+      return;
+    } else {
+      if(author) {
+        console.debug('ep_notesauth.setUsername: have authorid %s, setting username to %s', author, username);
+        authorManager.setAuthorName(author, username);
+      } else {
+        console.debug('ep_notesauth.setUsername: could not get authorid for token %s', token);
+      }
+    }
   });
 }
 
@@ -79,5 +98,22 @@ module.exports = {
     console.log('Auth failure for ' + context.req.path);
     context.res.send(401, 'Not authorized. Please <a href="http://www.uanotes.com/login">log in at uanotes.com</a> to continue.');
     callback([true]);
+  },
+  handleMessage: function(hook_name, context, cb) {
+    if(context.message.type == "CLIENT_READY") {
+      if(!context.message.token) {
+        console.debug('ep_notesauth.handleMessage: intercepted CLIENT_READY message has no token!');
+      } else {
+        var client_id = context.client.id;
+        var express_sid = context.client.manager.handshaken[client_id].sessionID;
+        var username = context.client.manager.handshaken[client_id].session.user.displayName;
+        console.debug('ep_notesauth.handleMessage: intercepted CLIENT_READY message for client_id = %s express_sid = %s, setting username for token %s to %s', client_id, express_sid, context.message.token, username);
+        setUsername(context.message.token, username);
+      }
+    } else if(context.message.type == "COLLABROOM" && context.message.data.type == "USERINFO_UPDATE") {
+      console.debug('ep_notesauth.handleMessage: intercepted USERINFO_UPDATE and dropping it!');
+      return cb([null]);
+    }
+    return cb([context.message]);
   }
 };
